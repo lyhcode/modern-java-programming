4c0398130ad8b413aa5c3c131161751e7af8edd1
lyhcode (Yan-hong Lin) <lyhcode@gmail.com>
2013-09-26 23:40:55 +0800

    增加範例

---
+++ examples/gradle/example001/src/main/java/com/example/Main.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class Main {
+
+    public static void main(String args[]) {
+        System.out.println(new Main().sayHello());
+    }
+
+    public String sayHello() {
+        return "Hello";
+    }
+}
